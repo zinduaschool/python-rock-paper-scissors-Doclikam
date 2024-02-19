5956a639f2d83245e9206dacb2ceebea69019234
Doclikam <likamwambui28@gmail.com>
2024-02-18 22:35:20 -0800

    fourth commit

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -4,106 +4,76 @@ import random
 
 # assigning variables
 
-
+options=('rock','paper','scissors')
 player_score=0
 cpu_score=0
 round_count=0
-tie_score=0
-out_of_round= False
 
 
+#loop runs when the round_count is less than 3.
 while round_count!=3:
 
-    computer_hand=random.choice(('rock','paper','scissors'))
+    computer_hand=random.choice(options)
 
     player_hand=input("Enter a choice('rock','paper','scissors'): ").lower()
-
-
-    def winner(player_hand, computer_hand):
-
-        if (player_hand=='rock' and computer_hand=='paper'):
-
-            print(f"you lose this round, {computer_hand} beats {player_hand}")
-
-            winner= 'cpu'
-
-        elif (player_hand=='rock' and computer_hand=='scissors'):
-
-            print(f"you win this round, {player_hand} beats {computer_hand}")
-
-            winner='player'
-
-        elif (player_hand=='paper' and computer_hand=='scissors'):
-
-            print(f"you lose this round, {computer_hand} beats {player_hand}")
-
-            winner= 'cpu'
-
-        elif (player_hand=='paper' and computer_hand=='rock'):
-
-            print(f"you win this round, {player_hand} beats{computer_hand}")
-
-            winner= 'player'
-
-        elif (player_hand=='scissors' and computer_hand=='rock'):
-
-            print(f"you lose this round, {computer_hand} beats {player_hand}")
-
-            winner= 'cpu'
-
-        elif (player_hand=='scissors' and computer_hand=='paper'):
-
-            print(f"you win this round, {player_hand} beats {computer_hand}")
-
-            winner='player'
-
-        else:
-
-            print(f"Its a tie, you picked {player_hand} and the cpu picked{computer_hand}")
-
-            winner= 'tie'
-
-        return winner
-
-    round_count=round_count+1
-    
 
     print(f"player: {player_hand}")
 
     print(f"computer: {computer_hand}")
 
-    print(winner(player_hand, computer_hand))
+    round_count=round_count+1
 
+    if (player_hand not in options):
+        print('invalid option, check spelling')
 
-while out_of_round:
-    if winner=='player':
+    elif (player_hand=='rock' and computer_hand=='paper'):
+
+        print(f"you lose this round, {computer_hand} beats {player_hand}")
+
+        cpu_score=cpu_score+1
+
+    elif (player_hand=='rock' and computer_hand=='scissors'):
+
+        print(f"you win this round, {player_hand} beats {computer_hand}")
+
         player_score=player_score+1
-        print(f"you score is {player_score}")  
-        if player_score==2:
-            print(f'you are the ultimate winner, your score is{player_score} ')
-            break
-    elif winner=='cpu':
+
+    elif (player_hand=='paper' and computer_hand=='scissors'):
+
+        print(f"you lose this round, {computer_hand} beats {player_hand}")
+
+        cpu_score=cpu_score+1
+
+    elif (player_hand=='paper' and computer_hand=='rock'):
+
+        print(f"you win this round, {player_hand} beats {computer_hand}")
+
         player_score=player_score+1
-        if cpu_score==2:
-            print(f'you lost you score is {player_score}')
-            break
+
+    elif (player_hand=='scissors' and computer_hand=='rock'):
+
+        print(f"you lose this round, {computer_hand} beats {player_hand}")
+
+        winner= 'cpu'
+        
+        cpu_score=cpu_score+1
+
+    elif (player_hand=='scissors' and computer_hand=='paper'):
+
+        print(f"you win this round, {player_hand} beats {computer_hand}")
+
+        player_score=player_score+1
     else:
-        tie_score=tie_score+1
-        
-        
+
+        print(f"Its a tie, you picked {player_hand} and the cpu picked {computer_hand}")
     
+    print(f'ýour score is {player_score}')
 
-    
-        
-
-
-    
-
-
-
- 
-
-
-
-
-
+#loop breaks if the player score or the computer is 2 and declares a winner. 
+while player_score<3 and cpu_score<3:
+    if (player_score==2):
+        print('ýou are the ultimate winner:)!!')
+        break
+    if (round_count==3):
+        print('computer won!!you are out of hands:(')
+        break
